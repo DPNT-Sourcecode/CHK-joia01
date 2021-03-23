1039640a0dc958c4219c1520e37d0bc225afcce7
sbx_user1051 <sbx_user1051@169.254.4.189>
2021-03-23 15:14:24 +0000

    Tue Mar 23 15:14:24 UTC 2021

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -3,13 +3,41 @@ package befaster.solutions.CHK;
 import befaster.runner.SolutionNotImplementedException;
 
 import java.util.HashMap;
+import java.util.Hashtable;
 
 public class CheckoutSolution {
+
+    class SkuValue {
+        String ident;
+        int value;
+        int multiplier;
+        int special_value;
+
+        public SkuValue(String ident, int value, int multiplier, int special_value) {
+            this.ident = ident;
+            this.value = value;
+            this.multiplier = multiplier;
+            this.special_value = special_value;
+        }
+    }
+
+    Hashtable<String, SkuValue> priceTable = new Hashtable<>(); // overkill for this, but for larger stuff ok
+
+    public CheckoutSolution()
+    {
+        priceTable.put("A", new SkuValue("A", 50, 3, 130));
+        priceTable.put("A", new SkuValue("B", 30, 2, 45));
+        priceTable.put("A", new SkuValue("C", 20, 3, 130));
+        priceTable.put("A", new SkuValue("A", 50, 3, 130));
+    }
+
     public Integer checkout(String skus) {
         // assume skus of form "ABBCD"
         // assume skus of form "A B B C D"
         // assume skus of form  "A,B,C,D"
         // assume skus of form  "A;B;C"
+
+
 
         String items[];
         HashMap<String, Integer> item_cnt = new HashMap<>(); // overkill for current example
@@ -29,10 +57,11 @@ public class CheckoutSolution {
             else item_cnt.put(items[i], cnt + 1);
         }
 
-        
+
 
     }
 }
+
 
 
 
