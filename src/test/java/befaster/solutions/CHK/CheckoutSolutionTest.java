6dfad492306186031a2769d1da392a4e317c7a1e
sbx_user1051 <sbx_user1051@169.254.4.189>
2021-03-23 18:42:12 +0000

    Tue Mar 23 18:42:12 UTC 2021

--- src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
+++ src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
@@ -8,73 +8,74 @@ public class CheckoutSolutionTest {
     @Test
     public void test1()
     {
-        Assert.assertEquals((long)(new CheckoutSolution().checkout("AABCDBAAEEAAAAA")), 525);
+        Assert.assertEquals(525, (long)(new CheckoutSolution().checkout("AABCDBAAEEAAAAA")));
     }
 
     @Test
     public void test2()
     {
-        Assert.assertEquals((long)(new CheckoutSolution().checkout("EE")), 80);
+        Assert.assertEquals(80, (long)(new CheckoutSolution().checkout("EE")));
     }
 
     @Test
     public void test3()
     {
-        Assert.assertEquals((long)(new CheckoutSolution().checkout("EEB")), 80);
+        Assert.assertEquals(80, (long)(new CheckoutSolution().checkout("EEB")));
     }
 
     @Test
     public void test4()
     {
-        Assert.assertEquals((long)(new CheckoutSolution().checkout("EEEB")), 120);
+        Assert.assertEquals(120, (long)(new CheckoutSolution().checkout("EEEB")));
     }
 
     @Test
     public void test5()
     {
-        Assert.assertEquals((long)(new CheckoutSolution().checkout("EEEBFF")), 140);
+        Assert.assertEquals(140, (long)(new CheckoutSolution().checkout("EEEBFF")));
     }
 
     @Test
     public void test6()
     {
-        Assert.assertEquals((long)(new CheckoutSolution().checkout("EEEBFFF")), 140);
+        Assert.assertEquals(140, (long)(new CheckoutSolution().checkout("EEEBFFF")));
     }
 
     @Test
     public void test7()
     {
-        Assert.assertEquals((long)(new CheckoutSolution().checkout("FFFFFFFF")), 60);
+        Assert.assertEquals(60, (long)(new CheckoutSolution().checkout("FFFFFFFF")));
     }
 
     @Test
     public void test8()
     {
-        Assert.assertEquals((long)(new CheckoutSolution().checkout("F")), 10);
+        Assert.assertEquals(10, (long)(new CheckoutSolution().checkout("F")));
     }
 
     @Test
     public void test9()
     {
-        Assert.assertEquals((long)(new CheckoutSolution().checkout("FF")), 20);
+        Assert.assertEquals(20, (long) (new CheckoutSolution().checkout("FF")));
     }
 
     @Test
     public void test10()
     {
-        Assert.assertEquals((long)(new CheckoutSolution().checkout("ABCDEF")), 165);
+        Assert.assertEquals(165, (long)(new CheckoutSolution().checkout("ABCDEF")));
     }
 
     @Test
     public void test11()
     {
-        Assert.assertEquals((long)(new CheckoutSolution().checkout("G")), 20);
+        Assert.assertEquals(20, (long)(new CheckoutSolution().checkout("G")));
     }
 
     @Test
     public void test12()
     {
-        Assert.assertEquals((long)(new CheckoutSolution().checkout("STXYZSTXYZ")), 152);
+        Assert.assertEquals(152, (long)(new CheckoutSolution().checkout("STXYZSTXYZ")));
     }
 }
+
 
