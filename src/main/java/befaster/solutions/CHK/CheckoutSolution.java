ec3675facd7b67972a399a81e5429622f578d77b
sbx_user1051 <sbx_user1051@169.254.4.189>
2021-03-23 15:05:24 +0000

    Tue Mar 23 15:05:24 UTC 2021

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -4,8 +4,20 @@ import befaster.runner.SolutionNotImplementedException;
 
 public class CheckoutSolution {
     public Integer checkout(String skus) {
-        
+        // assume skus of form "ABBCD"
+        // assume skus of form "A B B C D"
+        // assume skus of form  "A,B,C,D"
+        // assume skus of form  "A;B;C"
 
+        String items[];
+
+        if (skus.indexOf(" ") > 0) items = skus.split(" ");
+        else if (skus.indexOf(",") > 0) items = skus.split(",");
+        else if (skus.indexOf(";") > 0) items = skus.split(";");
+        else {
+            for (int i = 0; i < )
+        }
     }
 }
+
 
